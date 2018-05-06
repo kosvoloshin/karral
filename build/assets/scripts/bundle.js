468b2ae3d04d69f1611effbe99d104b1fb0bd6ae
Voloshin Konstantin <23.03@i.ua>
2018-05-06 17:08:04 +0300

    animate bg-city

--- build/assets/scripts/bundle.js
+++ build/assets/scripts/bundle.js
@@ -2392,3 +2392,52 @@ else
     window.SmoothScroll = SmoothScroll;
 
 })();
+
+/////////////////////////////////////////////////////////////////////////// анимация в секции услуги
+
+if($('.animate').length){
+    $("document").ready(function($){
+  
+        var animate = $('.animate');
+       
+        $(window).scroll(function () {
+          if ($(this).scrollTop() > $(".service").height()*0.8) {
+            animate.addClass("active");
+          }
+        });
+    });
+}
+
+/////////////////////////////////////////////////////////////////////////// паралакс в секции услуги
+
+if($('#animate').length){
+    var isMobile = false;
+    $(document).ready( function() {
+        if ($('body').width() <= 768) {
+            isMobile = true;
+        }
+        if (isMobile) {
+          console.log('mobile');
+        }
+        if (!isMobile) {
+          var parallaxContainer = document.getElementById('animate'),
+          layers = parallaxContainer.children;
+  
+          var moveLayers = function (e) {
+          var initialX = (window.innerWidth / 2) - e.pageX;
+          var initialY = (window.innerHeight / 2) - e.pageY;
+  
+          [].slice.call(layers).forEach(function(layer, index) {
+            var 
+              divider = index / 100,
+              positionX = initialX * divider,
+              transformString = 'translate(' + positionX + 'px)',
+              image = layer.firstElementChild;
+  
+            layer.style.transform = transformString;
+          });
+    };
+    window.addEventListener('mousemove', moveLayers);
+    }
+    });
+}
