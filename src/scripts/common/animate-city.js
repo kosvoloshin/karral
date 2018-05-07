7934bcff2575ff72f7ec92ad27e6daa6c99d41ec
Voloshin Konstantin <23.03@i.ua>
2018-05-07 20:10:02 +0300

    edits by objects

--- src/scripts/common/animate-city.js
+++ src/scripts/common/animate-city.js
@@ -1,14 +1,15 @@
 const $ = require('jquery');
-
-if($('.animate').length){
-  $("document").ready(function($){
-
-      var animate = $('.animate');
-     
-      $(window).scroll(function () {
-        if ($(this).scrollTop() > $(".service").height()*0.8) {
-          animate.addClass("active");
-        }
-      });
-  });
-}
+$(document).ready(function() {
+  if($('.animate').length){
+    $("document").ready(function($){
+  
+        var animate = $('.animate');
+       
+        $(window).scroll(function () {
+          if ($(this).scrollTop() > $(".service").height()*0.8) {
+            animate.addClass("active");
+          }
+        });
+    });
+  }
+});
--- src/scripts/common/parallax.js
+++ src/scripts/common/parallax.js
@@ -1,33 +1,34 @@
 const $ = require('jquery');
+$(document).ready(function() {
+  if($('#animate').length){
+    var isMobile = false;
+    $(document).ready( function() {
+        if ($('body').width() <= 768) {
+            isMobile = true;
+        }
+        if (isMobile) {
+          //console.log('mobile');
+        }
+        if (!isMobile) {
+          var parallaxContainer = document.getElementById('animate'),
+          layers = parallaxContainer.children;
 
-if($('#animate').length){
-  var isMobile = false;
-  $(document).ready( function() {
-      if ($('body').width() <= 768) {
-          isMobile = true;
-      }
-      if (isMobile) {
-        console.log('mobile');
-      }
-      if (!isMobile) {
-        var parallaxContainer = document.getElementById('animate'),
-        layers = parallaxContainer.children;
+          var moveLayers = function (e) {
+          var initialX = (window.innerWidth / 2) - e.pageX;
+          var initialY = (window.innerHeight / 2) - e.pageY;
 
-        var moveLayers = function (e) {
-        var initialX = (window.innerWidth / 2) - e.pageX;
-        var initialY = (window.innerHeight / 2) - e.pageY;
+          [].slice.call(layers).forEach(function(layer, index) {
+            var 
+              divider = index / 100,
+              positionX = initialX * divider,
+              transformString = 'translate(' + positionX + 'px)',
+              image = layer.firstElementChild;
 
-        [].slice.call(layers).forEach(function(layer, index) {
-          var 
-            divider = index / 100,
-            positionX = initialX * divider,
-            transformString = 'translate(' + positionX + 'px)',
-            image = layer.firstElementChild;
-
-          layer.style.transform = transformString;
-        });
-  };
-  window.addEventListener('mousemove', moveLayers);
+            layer.style.transform = transformString;
+          });
+    };
+    window.addEventListener('mousemove', moveLayers);
+    }
+    });
   }
-  });
-}
+});
